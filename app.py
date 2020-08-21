5929edaef9d894827175cac8608e177a0d819d02
Elias Chen <elchen0727@gmail.com>
2020-08-20 21:42:30 -0700

    cleaned up code and made it so the bot retweets with a comment

---
+++ app.py
@@ -0,0 +1,54 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+class TwitterBot:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.bot = webdriver.Chrome("C:\Program Files (x86)\chromedriver.exe")
+    
+    def login(self):
+        bot = self.bot
+        bot.get("https://www.twitter.com/login")
+        email = bot.find_element_by_name("session[username_or_email]")
+        email.send_keys(self.username)
+        password = bot.find_element_by_name("session[password]")
+        password.send_keys(self.password)
+        password.send_keys(Keys.RETURN)
+
+    def findTweet(self):
+        bot = self.bot
+        bot.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+        time.sleep(2)
+        tweets = bot.find_elements_by_xpath("//div[@lang='en']")
+        
+        for tweet in tweets:
+            contents = tweet.text
+            if "update:" in contents:
+                covidUpdate = tweet
+                cases = contents[17:20]
+                print(cases)
+                break
+        covidUpdate.click()
+        return bot.current_url
+    
+    def retweetUpdate(self, link):
+        bot = self.bot
+        retweetMenu = bot.find_element_by_xpath("//div[@lang='en']//parent::div//following::div[4]")
+        retweetMenu.find_element_by_xpath("//div[@data-testid='retweet']").click()
+        time.sleep(3)
+        bot.find_element_by_xpath("//a[@href='/compose/tweet']").click()
+        time.sleep(2)
+        comment = bot.find_element_by_xpath("//div[@role='textbox']")
+        comment.send_keys("Stay home people!")
+        
+
+
+CovidBot = TwitterBot("nineteenc82@gmail.com", "FUCKCovid19")
+CovidBot.login()
+tweet_link = CovidBot.findTweet()
+CovidBot.retweetUpdate(tweet_link)
+
+
+
